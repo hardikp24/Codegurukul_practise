12b5275b79e00080e272cb418b000d9d3c9e734b
hardikp24 <hardik.patil2494@gmail.com>
2015-06-24 10:24:08 +0530

    added view

--- server.js
+++ server.js
@@ -1,17 +1,87 @@
-var express = require('express'); //node module
-var app = express();
+var express = require('express'); //Node Module for express server
+var mongoose = require('mongoose'); //Node Module for mongoose server
+var bodyParser = require('body-parser'); //Node Module for BP server
 
-app.set('views',__dirname+'/server/views' );
-//console.log(__dirname+"This is variable");
+var app=express();
 
+app.set('views',__dirname+'/server/views');
+console.log(__dirname+"This is a variable");
 app.set('view engine','jade');
+app.use(express.static('public')); //static route handling
 
-//app.use is used to use middleware
-app.use(express.static('public'));
+//Routes
+app.use(bodyParser.json());// POST: {"name":"foo","color":"red"}
+app.use(bodyParser.urlencoded({extended:true}));// assuming POST:name=foo&color=red<--URL encoding
+app.get('/',function(req,res)
+{
+
+res.render('index',{courseList:courses});
+
+});
+
+app.get('/addcourse',function(req,res)
+{
+
+res.render('addcourse',{courseList:courses});
+
+});
 
 
 
-//JSON array of objects
+
+
+app.post('/addcourse',function(req,res)
+{
+// res.render('addcourse',{courseList:courses});
+ // res.send("Data Recieved");
+//Create a new course
+var course=new Course ({name:req.body.coursename,published:req.body.date,featured:req.body.feat});
+//The Magic!
+course.save(function(err, doc){
+		if(err) res.json(err);
+		else    res.redirect('/view');
+	});
+// res.render('/view');
+res.send("Done!");
+});
+
+app.get('/view', function(req, res){
+	Course.find({}, function(err, docs){
+		if(err) res.json(err);
+		else    res.render('view', {Course: docs});
+	});
+});
+ 
+// app.get('/view',function(req,res)
+// {
+
+// Course.find({},function()
+// {
+// });
+// });
+
+
+
+
+// Mongoose Connection with MongoDB
+mongoose.connect("mongodb://localhost/codegurukul");
+console.log("local mongodb opened");
+
+//Mongoose schema
+var courseSchema = new mongoose.Schema({
+name:String,
+featured:String,
+published:String
+});
+
+//Compile schema into a mongoose model
+var Course = mongoose.model('Course',courseSchema);
+
+// var fetchCourse=Course.find();//retrieve all the values 
+
+
+
+
 var courses = [
 {"name": "C# for Sociopaths", "featured": true, "published": new Date('10/5/2013')},
 {"name": "C# for Non-Sociopaths", "featured": true, "published": new Date('10/12/2013')},
@@ -30,15 +100,19 @@ var courses = [
 {"name": "Death March Coding for Fun and Profit", "featured": true, "published": new Date('7/1/2013')}
 ];
 
-//req= request => HTTP REQUEST obj
-//res= request => HTTP Response obj
-
-//route
-
-app.get('/', function(req,res){
-res.render('index',{courseList:courses}); 
-});
 
 
-app.listen('3020');
-console.log('Express is listen to localhost 3020');
+// app.get('/login',function(req,res)
+// {
+
+// res.render('login');
+
+// });
+//req=>request res=>response
+
+
+
+
+
+app.listen(3020);
+console.log("Express Server is listening at port 3020");
