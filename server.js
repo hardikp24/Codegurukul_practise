ec639de9d8be9b00a283283b3171284be5b18a30
hardikp24 <hardik.patil2494@gmail.com>
2015-06-25 09:44:41 +0530

    uploaded login page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser'); //Node Module for BP server
 var app=express();
 
 app.set('views',__dirname+'/server/views');
-console.log(__dirname+"This is a variable");
+console.log(__dirname+"   This is a variable");
 app.set('view engine','jade');
 app.use(express.static('public')); //static route handling
 
@@ -21,66 +21,112 @@ res.render('index',{courseList:courses});
 
 app.get('/addcourse',function(req,res)
 {
-
 res.render('addcourse',{courseList:courses});
-
 });
 
 
 
-
-
-app.post('/addcourse',function(req,res)
-{
+app.post('/addcourse',function(req,res) {
 // res.render('addcourse',{courseList:courses});
  // res.send("Data Recieved");
 //Create a new course
-var course=new Course ({name:req.body.coursename,published:req.body.date,featured:req.body.feat});
-//The Magic!
-course.save(function(err, doc){
-		if(err) res.json(err);
-		else    res.redirect('/view');
-	});
-// res.render('/view');
-res.send("Done!");
+
+var course=new Course ({uname:req.body.uname,
+						name:req.body.coursename,
+						published:req.body.date,
+						featured:req.body.feat,
+						time:req.body.timer,
+						batch:req.body.batches
+					});
+
+course.save(function(err){
+                Course.find(function(err,courses){
+            res.render('view-course',{courses:courses});
+        });
+    });
 });
 
-app.get('/view', function(req, res){
-	Course.find({}, function(err, docs){
-		if(err) res.json(err);
-		else    res.render('view', {Course: docs});
-	});
+
+  
+
+app.post('/deletecourse/:id',function(req,res){
+        Course.remove({ _id:req.params.id }, function (err) {
+            Course.find(function(err,courses){
+            res.render('view-course',{courses:courses});
+        });
+    });
 });
- 
-// app.get('/view',function(req,res)
-// {
-
-// Course.find({},function()
-// {
-// });
-// });
 
 
 
-
-// Mongoose Connection with MongoDB
+ // Mongoose Connection with MongoDB
 mongoose.connect("mongodb://localhost/codegurukul");
 console.log("local mongodb opened");
 
 //Mongoose schema
 var courseSchema = new mongoose.Schema({
 name:String,
-featured:String,
-published:String
+featured:Boolean,
+published:Date,
+time:String,
+batch:String
 });
 
 //Compile schema into a mongoose model
 var Course = mongoose.model('Course',courseSchema);
 
+//User Schema 
+var userSchema = new mongoose.Schema({
+		name:String,
+		username:String,
+		password: String
+});
+
+var User = mongoose.model('User',userSchema);
+
+
+
+ app.get('/signup',function(req,res){
+res.render('signup');
+ 
+        });
+
+
+app.post('/signup',function(req,res){
+
+
+ var user = new User ({name: req.body.uname, username:req.body.uid,password:req.body.pass});
+user.save(function(err){
+                User.find(function(err){
+            res.render('index',{user:user});
+        });
+        });
+        });
+app.post('/login',function(req,res)
+{
+    User.findOne({username:req.body.username},function(err,user){
+        if(user)
+        {
+            if(user.password==req.body.password)
+                res.send('loggedin');
+            else
+                res.send('passsword incorrect');
+        }
+        else
+        {
+            res.send('User not Defind'+req.body.username);
+
+        }
+    });
+    });
+
+
+
+
+
+
+
 // var fetchCourse=Course.find();//retrieve all the values 
-
-
-
 
 var courses = [
 {"name": "C# for Sociopaths", "featured": true, "published": new Date('10/5/2013')},
